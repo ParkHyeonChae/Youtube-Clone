fda8e6e6a71fb22653f8f85a51fb5aeb58699cdb
ParkHyeonChae <park19996@naver.com>
2020-06-25 03:15:19 +0900

    Side Video 출력

--- client/src/components/views/VideoDetailPage/Sections/SideVideo.js
+++ client/src/components/views/VideoDetailPage/Sections/SideVideo.js
@@ -1,23 +1,57 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Axios from 'axios';
+
 
 function SideVideo() {
-    return (
-        <div style={{ display:'flex', marginBottom: '1rem', padding: '0 2rem' }}>
-            <div style={{ width: '40%', marginBottom: '1rem' }}>
+
+    const [sideVideos, setsideVideos] = useState([])
+
+    useEffect(() => {
+        
+        Axios.get('/api/video/getVideos')
+            .then(response => {
+                if(response.data.success) {
+                    console.log(response.data)
+                    setsideVideos(response.data.videos)
+                } else {
+                    alert('Get Video Failed...')
+                }
+            })
+
+    }, [])
+
+
+    const renderSideVideo = sideVideos.map((video, index) => {
+
+        var minutes = Math.floor(video.duration / 60);
+        var seconds = Math.floor(video.duration - minutes * 60);
+
+        return <div key={index} style={{ display:'flex', marginBottom: '1rem', padding: '0 2rem' }}>
+            <div style={{ width: '40%', margitRight: '1rem' }}>
                 <a href >
-                    <img style={{ width: '100%' }} src alt />
+                    <img style={{ width: '100%', height: '100%' }} src={`http://localhost:5000/${video.thumbnail}`} alt="thumbnail" />
                 </a>
             </div>
 
-            <div style={{ width: '50%' }}>
-                <a href>
-                    <span style={{ fontSize: '1rem', color: 'black' }}>VideoTitle</span>
-                    <span>videoWriterName</span><br />
-                    <span>videoViews</span><br />
-                    <span>Time</span>
+            <div style={{ width: '50%', marginLeft: '1rem' }}>
+                <a href style={{ color: 'gray' }}>
+                    <span style={{ fontSize: '1rem', color: 'black' }}>{video.title}</span><br />
+                    <span>{video.writer.name}</span><br />
+                    <span>{video.views} views</span><br />
+                    <span>{minutes} : {seconds}</span>
                 </a>
             </div>
         </div>
+
+    })
+
+    return (
+        
+        <React.Fragment>
+            <div style={{ marginTop: '3rem' }} />
+                {renderSideVideo}
+
+        </React.Fragment>
     )
 }
 
