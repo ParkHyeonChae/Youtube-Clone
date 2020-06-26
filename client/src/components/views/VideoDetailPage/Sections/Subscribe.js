ba2071ef90b0b24ec19d14f5395c5ffa7bd00bfc
ParkHyeonChae <park19996@naver.com>
2020-06-27 00:18:25 +0900

    구독자 수, 구독 유무 판별 route 생성, API연결

--- client/src/components/views/VideoDetailPage/Sections/Subscribe.js
+++ client/src/components/views/VideoDetailPage/Sections/Subscribe.js
@@ -1,17 +1,50 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Axios from 'axios'
 
-function Subscribe() {
+
+function Subscribe(props) {
+
+    const [SubscribeNumber, setSubscribeNumber] = useState(0)
+    const [Subscribed, setSubscribed] = useState(false)
+
+    useEffect(() => {
+    
+        let variable = { userTo: props.userTo }
+        
+        Axios.post('/api/subscribe/subscriberNumber', variable) // 구독자 수 정보
+            .then(response => {
+                if(response.data.success) {
+                    setSubscribeNumber(response.data.SubscribeNumber)
+                } else {
+                    alert('Failed Get Subscribes...')
+                }
+            })
+        
+        let subscribedVariable = { userTo: props.userTo, useFrom: localStorage.getItem('userId') } // 영상 게시자를 내가 구독하는 확인, 클라 LocalStorage에서 가져옴
+        
+
+        Axios.post('/api/subscribe/subscribed', subscribedVariable)
+            .then(response => {
+                if(response.data.success) {
+                    setSubscribed(response.data.subscribed)
+                } else {
+                    alert('Failed Get Data...')
+                }
+            })
+
+    }, [])
+
     return (
         <div>
             <button
                 style={{
-                    backgroundColor: '#CC0000', borderRadius: '4px',
+                    backgroundColor: `${Subscribe ? '#CC0000' : '#AAAAAA'}`, borderRadius: '4px',
                     color: 'white', padding: '10px 16px',
                     fontWeight: '500', fontSize: '1rem', textTransform: 'uppercase'
                 }}
                 onClick
             >
-                0 Subscribe
+                {SubscribeNumber} {Subscribed ? 'Subscribed' : 'Subscribe'}
             </button>
         </div>
     )
---
+++ server/routes/subscribe.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const router = express.Router();
+const { Subscriber } = require("../models/Subscriber");
+
+
+//=================================
+//            Subscribe
+//=================================
+
+
+router.post('/subscribeNumber', (req, res) => {
+   
+    Subscriber.find({ 'userTo': req.body.userTo })
+        .exec((err, subscribe) => {
+            if(err) return res.status(400).send(err);
+            return res.status(200).json({ success: true, subscribeNumber: subscribe.length })
+        })
+
+});
+
+
+router.post('/subscribed', (req, res) => {
+   
+    Subscriber.find({ 'userTo': req.body.userTo, 'userFrom': req.body.userFrom })
+        .exec((err, subscribe) => { // subscribe.length가 1만되면 내가 이사람을 구독하고 있다, data가 없으면 구독하고 있지 않다
+            if(err) return res.status(400).send(err);
+            let result = false
+            if(subscribe.length !== 0) {
+                result = true
+            }
+            res.status(200).json({ success: true, subscribed: result })
+        })
+
+});
+
+
+module.exports = router;
