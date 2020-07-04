e2ffdf6f419ce764868dd0e2d9badda2a15e0450
ParkHyeonChae <park19996@naver.com>
2020-07-04 21:50:55 +0900

    Comment OnSubmit func 구현, console 출력

--- client/src/components/views/VideoDetailPage/Sections/Comment.js
+++ client/src/components/views/VideoDetailPage/Sections/Comment.js
@@ -1,13 +1,37 @@
 import React, { useState } from 'react'
+import Axios from 'axios';
+import { useSelector } from 'react-redux';
 
-function Comment() {
 
+function Comment(props) {
+
+    const videoId = props.postId // 부모컴퍼넌트(VideoDetailPage)에서 넘겨받음
+    const user = useSelector(state => state.user); // Redux안에 있는 State에서 가져온 user를 user변수에 넣음
     const [commentValue, setcommentValue] = useState("")
 
     const handleClick = (event) => {
         setcommentValue(event.currentTarget.value)
     }
 
+    const onSubmit = (event) => {
+        event.preventDefault(); // 새로고침 방지
+
+        const variables = {
+            content: commentValue,
+            writer: user.userData._id, // 이 부분은 LocalStorage가 아닌 Redux에서 가져옴
+            postId: videoId
+        }
+
+        Axios.post('/api/comment/saveComment', variables)
+            .then(response => {
+                if(response.data.success) {
+                    console.log(response.data.result)
+                } else {
+                    alert('Failed Comment Save...')
+                }
+            })
+
+    }
 
     return (
         <div>
@@ -19,7 +43,7 @@ function Comment() {
 
             {/* Root Comment Form */}
 
-            <form style={{ display: 'flex' }} onSubmit>
+            <form style={{ display: 'flex' }} onSubmit={onSubmit}>
                 <textarea
                     style={{ width: '100%', borderRadius:' 5px' }}
                     onChange={handleClick}
@@ -28,7 +52,7 @@ function Comment() {
                 />
                 <br />
 
-                <button style={{ width: '20%', height: '52px' }} onClick>Submit</button>
+                <button style={{ width: '20%', height: '52px' }} onClick={onSubmit}>Submit</button>
             </form>
 
         </div>
--- client/src/components/views/VideoDetailPage/VideoDetailPage.js
+++ client/src/components/views/VideoDetailPage/VideoDetailPage.js
@@ -48,7 +48,7 @@ function VideoDetailPage(props) {
                     </List.Item>
     
                     {/* Comments */}
-                    <Comment />
+                    <Comment postId={videoId}/>
     
                 </div>
                 
