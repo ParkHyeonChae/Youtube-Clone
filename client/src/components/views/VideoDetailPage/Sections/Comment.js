0b16045b2d1aa1febfa70d2ac1d576a2d4225bb7
ParkHyeonChae <park19996@naver.com>
2020-07-04 21:29:14 +0900

    Comment Templates 작성

--- client/src/components/views/VideoDetailPage/Sections/Comment.js
+++ client/src/components/views/VideoDetailPage/Sections/Comment.js
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Comment() {
+
+    const [commentValue, setcommentValue] = useState("")
+
+    const handleClick = (event) => {
+        setcommentValue(event.currentTarget.value)
+    }
+
+
     return (
         <div>
-            
+            <br />
+            <p>Replies</p>
+            <hr />
+
+            {/* Comment Lists */}
+
+            {/* Root Comment Form */}
+
+            <form style={{ display: 'flex' }} onSubmit>
+                <textarea
+                    style={{ width: '100%', borderRadius:' 5px' }}
+                    onChange={handleClick}
+                    value={commentValue}
+                    placeholder="Please Write Comments..."
+                />
+                <br />
+
+                <button style={{ width: '20%', height: '52px' }} onClick>Submit</button>
+            </form>
+
         </div>
     )
 }
